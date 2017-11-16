bff8983c9675474772dc32c2abad26aa31d3bc3c
TedtheDev <tylerjedmunds@gmail.com>
2017-11-16 15:32:08 -0600

    added ability to connect to bluetooth devices and scan devices and disconnect

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { MyApp } from './app.component';
 import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
-import { SettingsPage } from '../pages/settings/settings';
+import { ConnectRobotPage } from '../pages/connectRobot/connectRobot';
 import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
@@ -19,7 +19,7 @@ import { BluetoothSerial } from '@ionic-native/bluetooth-serial';
     AboutPage,
     ContactPage,
     HomePage,
-    SettingsPage,
+    ConnectRobotPage,
     TabsPage
   ],
   imports: [
@@ -32,7 +32,7 @@ import { BluetoothSerial } from '@ionic-native/bluetooth-serial';
     AboutPage,
     ContactPage,
     HomePage,
-    SettingsPage,
+    ConnectRobotPage,
     TabsPage
   ],
   providers: [
